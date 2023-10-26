05a535ef2f7bec7120a9b06478465e2dabb7e1e0
Alina Avanesyan <90269742+alinaavanesyan@users.noreply.github.com>
2023-10-27 02:42:58 +0300

    Update urls.py

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from myapp2.views import search
 
 urlpatterns = [
-    path('', search, name='search'),
+    path('',  search, name='search'),
+    path('/<str:query>/<int:page>',  search, name='search'),
 ]

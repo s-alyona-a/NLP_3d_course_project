a31f0ec6e61059588814444dfa3998e2d1c029ed
Alina Avanesyan <90269742+alinaavanesyan@users.noreply.github.com>
2023-10-27 15:39:54 +0300

    Update views.py

--- myproject/myapp2/views.py
+++ myproject/myapp2/views.py
@@ -149,6 +149,82 @@ def search_func(search_first):
                 res.append([all, sentences_objects.filter(nmb_text=text_id).values_list('author', flat=True)[0], sentences_objects.filter(nmb_text=text_id).values_list('title', flat=True)[0]])
             text_id+=1
             spis = {}
+    
+    elif (search_first[0]=='"' and search_first[-1]!='"') or (search_first[0]!='"' and search_first[-1]=='"'):
+        res = []
+        search = search_first.split()
+        text_id = 1
+        while text_id <= words_objects.aggregate(Max('nmb_text'))['nmb_text__max']:
+            df = words_objects.filter(nmb_text=text_id)
+            sent_ind = 1
+            all = []
+            spis = {}
+            while sent_ind <= df.aggregate(Max('nmb_text'))['nmb_text__max']:
+                srch_i = 0
+                words = df.filter(nmb_sent=sent_ind).values_list('word', flat=True)
+                tags = df.filter(nmb_sent=sent_ind).values_list('pos', flat=True)
+                lemmas = df.filter(nmb_sent=sent_ind).values_list('lemma', flat=True)
+                word_i = 0
+                spis_n = []
+                while word_i<len(words) and srch_i<len(search):
+                    if search[srch_i][0]=='"' and search[srch_i][-1]=='"':
+                        if '+' in search[srch_i]:
+                            d = search[srch_i].split('+')
+                            if d[0]==words[word_i] and d[1]==tags[word_i]:
+                                spis_n.append(words[word_i])
+                                srch_i+=1
+                            else:
+                                word_i -=len(spis_n)
+                                srch_i = 0
+                                spis_n = []
+                        elif words[word_i] == search[srch_i]:
+                            spis_n.append(words[word_i])
+                            srch_i+=1
+                        elif tags[word_i] == search[srch_i]:
+                            spis_n.append(words[word_i])
+                            srch_i+=1
+                        else:
+                            word_i -=len(spis_n)
+                            srch_i = 0
+                            spis_n = []
+                    elif search[srch_i][0]!='"' and search[srch_i][-1]!='"':
+                        lem = m.parse(search[srch_i])[0].normal_form
+                        if '+' in search[srch_i]:
+                            d = search[srch_i].split('+')
+                            if m.parse(d[0])[0].normal_form==lemmas[word_i] and d[1]==tags[word_i]:
+                                spis_n.append(words[word_i])
+                                srch_i+=1
+                            else:
+                                word_i -=len(spis_n)
+                                srch_i = 0
+                                spis_n = []
+                        elif lemmas[word_i] == lem:
+                            spis_n.append(words[word_i])
+                            srch_i+=1
+                        elif tags[word_i] == search[srch_i]:
+                            spis_n.append(words[word_i])
+                            srch_i+=1
+                        else:
+                            word_i -=len(spis_n)
+                            srch_i = 0
+                            spis_n = []
+                    if spis_n!=[] and len(search) == len(spis_n):
+                        if sent_ind not in spis.keys():
+                            spis[sent_ind] = []
+                            spis[sent_ind].append(' '.join(spis_n))
+                        else:
+                            spis[sent_ind].append(' '.join(spis_n))
+                        srch_i = 0
+                        word_i -=len(spis_n)
+                        word_i += 1
+                        spis_n = []
+                    word_i+=1
+                sent_ind+=1   
+            all = [[spis[k], sentences_objects.filter(nmb_text=text_id).filter(nmb_sent=k).values_list('sentence', flat=True)[0]] for k in spis.keys()]
+            if all != []:
+                res.append([all, sentences_objects.filter(nmb_text=text_id).values_list('author', flat=True)[0], sentences_objects.filter(nmb_text=text_id).values_list('title', flat=True)[0]])
+            text_id+=1
+            spis = {}    
     return res
 
 def search(request):
